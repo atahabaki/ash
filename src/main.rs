2fb8e671fb2ad6f03f9daa614cbb17eb088dba9c
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-09-03 00:41:09 +0300

    ✨ run it if it's just 1 word XD...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process::Command;
+
 fn main() {
     println!("Yeah!.. Let's crack the shell!..");
+    loop {
+        let mut cmd = String::new();
+        if let Ok(size) = std::io::stdin().read_line(&mut cmd) {
+            let cmd = cmd.trim_matches(char::is_whitespace);
+            if ! cmd.contains(char::is_whitespace) {
+                let mut program= Command::new(cmd);
+                match program.spawn() {
+                    Ok(child) => {
+                        #[cfg(debug_assertions)]
+                        println!("Launched: {:?}!..", child);
+                    },
+                    Err(e) => panic!("Couldn't find {} in PATH.\n{}", &cmd[..], e)
+                }
+            }
+        }
+    }
 }
